b7548c8386dc06294a781cf3ab87188630d35228
Satara <tatenokay@gmail.com>
2023-06-13 02:47:03 -0500

    NextJS

---
+++ components/PageLayout.jsx
@@ -0,0 +1,31 @@
+import Head from "next/head"
+import Link from "next/link"
+export default function PageLayout({children}){
+  return(
+    <>
+    <Head>
+      <title>NewApp</title>
+      <meta  name="description" content="newapp - blallabla bla"/>
+      <link rel="icon" href=""/>
+    </Head>
+    <header>
+      <div>
+        <Link href='/'>newsapi</Link>
+      </div>
+      <div>
+        <Link href='../pages/about.js'>ir About</Link>
+      </div>
+    </header>
+    <main>
+      {children}
+    </main>
+    <style jsx>{`
+    header {
+      display : flex;
+      justify-content: space-between;
+      padding: 20px;
+      }
+    `}</style>
+    </>
+  )
+}
